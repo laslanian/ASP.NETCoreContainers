ffbf5b6c837b5ec65aad6a52a34a49950cb6a067
Levon Aslanian <leoaslanian13@hotmail.com>
2017-10-24 07:53:20 -0400

    CI trial

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace WebApplication2.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page. Add your contacts here";
 
             return View();
         }
